83ff1d99a4d44cad01a9ed86dabf44b85293bb5a
Vaibhavi Sutar <vaibhavi7029@gmail.com>
2026-05-05 11:31:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 
+// Scroll animation
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+    sections.forEach(sec => {
+        let top = sec.getBoundingClientRect().top;
+        let screen = window.innerHeight;
+
+        if(top < screen - 100){
+            sec.classList.add("show");
+        }
+    });
+});
+
+// Theme toggle
+function toggleTheme(){
+    document.body.classList.toggle("light");
+}
+
+// Scroll to contact
+function scrollToContact(){
+    document.getElementById("contact").scrollIntoView({behavior:"smooth"});
+}
+
+// Form
+document.getElementById("form").addEventListener("submit",(e)=>{
+    e.preventDefault();
+    document.getElementById("msg").innerHTML="Message Sent ✅";
+});
